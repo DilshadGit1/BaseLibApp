d86018f9570add8722991fa3eeeabf0a47ddf972
dilshadlgf <dilshad.ahmad@logikoof.in>
2024-01-24 17:29:05 +0530

    1.0.0

--- baseLibv1/src/main/java/com/example/municipal/data/network/BaseUrlProvider.kt
+++
@@ -1,6 +0,0 @@
-package com.example.municipal.data.network
-
-interface BaseUrlProvider {
-    fun getBaseUrl(): String
-    fun setBaseUrl(string: String)
-}
--- baseLibv1/src/main/java/com/example/municipal/data/network/OkHttpClientProvider.kt
+++
@@ -1,7 +0,0 @@
-package com.example.municipal.data.network
-
-import okhttp3.OkHttpClient
-
-interface OkHttpClientProvider {
-    fun get(): OkHttpClient
-}
