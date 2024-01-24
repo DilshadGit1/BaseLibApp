cecc36ab9830770b516fced5a5d23aab82839bed
dilshadlgf <dilshad.ahmad@logikoof.in>
2024-01-24 15:08:41 +0530

    1.0.0

---
+++ baseLibv1/src/main/java/com/example/municipal/data/network/MyLibraryConfig.kt
@@ -0,0 +1,5 @@
+package com.example.municipal.data.network
+
+interface MyLibraryConfig {
+    val baseUrl: String
+}
---
+++ baseLibv1/src/main/java/com/example/municipal/data/network/MyLibraryConfigImpl.kt
@@ -0,0 +1,3 @@
+package com.example.municipal.data.network
+
+class MyLibraryConfigImpl(override val baseUrl: String) : MyLibraryConfig
